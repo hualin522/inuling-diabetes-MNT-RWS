e00827cc9cca0043fdf0058afdf25fe56092c918
hualin522 <wanghualin313@163.com>
2026-05-10 22:26:29 +0800

    Add files via upload

--- patient_info_entry.py
+++ patient_info_entry.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from langchain_community.document_loaders import PyPDFDirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.chains import create_retrieval_chain, create_stuff_documents_chain
